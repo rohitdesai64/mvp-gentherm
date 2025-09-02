3894c5a0e62de6396306a7dada0f46cbb5708101
Rohit <rohit.desai64@gmail.com>
2025-09-02 23:59:20 +0530

    images style updated

--- src/ImgModal.js
+++ src/ImgModal.js
@@ -2,15 +2,15 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import { Modal, Stack } from "@mui/material";
-import sample1 from "./img/sample1.svg";
-import sample2 from "./img/sample2.jpg";
+import sample1 from "./img/sample3.jpg";
+import sample2 from "./img/sample4.jpg";
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: "60%",
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
